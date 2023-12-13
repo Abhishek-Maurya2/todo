de64a60fd71f2f10388a80c9e4e42e409662de8a
Abhishek-Maurya2 <208akmaurya@hmail.com>
2023-12-13 18:06:37 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TodosComponent } from './MyComponent/todos/todos.component';
+import { TodoItemComponent } from './MyComponent/todo-item/todo-item.component';
+import { AddTodoComponent } from './MyComponent/add-todo/add-todo.component';
+import { AboutComponent } from './MyComponent/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo';
